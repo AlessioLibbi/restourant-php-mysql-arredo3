93022b71519abdba5e50f75efeff235d9c4c7822
Alessio Libbi <alex.libbi@gmail.com>
2023-10-25 17:13:21 +0200

    Add:votes table added

--- index.php
+++ index.php
@@ -29,8 +29,47 @@ function init() {
     if($result) {
         echo "Tabella creata";
     }
-    $seeder_restaurant = 'INSERT INTO restaurant (name, address, phone, opening_time) VALUES ("Ristorante da Mario", "Via Roma, 123", "+39 123 456789", "{ )'
+    $populate_restaurant_table = "
+        INSERT INTO restaurant (name, address, phone, opening_time)
+        VALUES ('Ristorante da Mario','Via Roma, 123','+39 123 456789','{\"lunedì\" : \"09:00 - 22:00\",
+        \"martedì\" : \"09:00 - 22:00\",
+        \"mercoledì\" : \"09:00 - 22:00\",
+        \"giovedì\" : \"09:00 - 22:00\",
+        \"venerdì\" : \"09:00 - 23:00\",
+        \"sabato\" : \"10:00 - 23:00\",
+        \"domenica\" : \"10:00 - 21:00\"
+        }')
+        ";
+    // var_dump($seeder_restaurant);
+    $pdo->exec($populate_restaurant_table);
 
+    $create_vote_table = "CREATE TABLE IF NOT EXISTS `votes`(
+        `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+        `client_name` VARCHAR(40) NOT NULL,
+        `vote` INT(10) NOT NULL,
+        `comment` VARCHAR(60),
+        PRIMARY KEY(`id`))
+        ENGINE = InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_general_ci;";
+    $pdo->exec($create_vote_table);
+
+    $populate_vote_table = "
+        INSERT INTO votes (client_name, vote, comment)
+        VALUES ";
+        
+     $pdo->exec($populate_vote_table);
+     $votes_data = file_get_contents("recensioni.json");
+     $votes_decoded = json_decode($votes_data);
+
+     foreach($votes_decoded->recensioni as $key => $vote) {
+        $populate_vote_table .= "($vote->nome_cliente, $vote->voto, $vote->commento)";
+        if($key !== array_key_last($votes_decoded->recensioni)) {
+            $populate_vote_table .= ", ";
+        }
+     }
+     $populate_vote_table .= ";";
+     $pdo->exec($populate_vote_table);
+     
+     
 
 }
 switch ($_POST["action"]) {
